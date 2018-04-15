60aaa1effb4a81f6783d31b54913c1d13c630f0b
Azurolu <33943251+Azurolu@users.noreply.github.com>
2018-04-15 23:30:50 +0200

    Update prepro.py

--- my_code/prepro.py
+++ my_code/prepro.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python2
+    #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+"""
+@author: isabelleguyon
 
-from sys import argv, path
-path.append ("../../ingestion_program") # Contains libraries you will need
-from data_manager import DataManager  # such as DataManager
-import numpy as np
+This is an example of program that preprocesses data.
+It calls the PCA function from scikit-learn.
+Replace it with programs that:
+    normalize data (for instance subtract the mean and divide by the standard deviation of each column)
+    construct features (for instance add new columns with products of pairs of features)
+    select features (see many methods in scikit-learn)
+    perform other types of dimensionality reductions than PCA
+    remove outliers (examples far from the median or the mean; can only be done in training data)
+"""
+
 from sklearn.base import BaseEstimator
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.feature_selection import SelectFromModel
-from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
-from sklearn.preprocessing import Imputer
-from sklearn.manifold import LocallyLinearEmbedding
-from sklearn.cluster import FeatureAgglomeration
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import VarianceThreshold,SelectFromModel
+from sklearn.ensemble import RandomForestRegressor,BaggingRegressor
 
 class NothingTransformer:
     def fit(self, X, y):
@@ -23,67 +26,96 @@ class NothingTransformer:
     def transform(self, X):
         return X
     def __str__(self) :
-        return "No transforming needed."
+        return "Don't worry : Nothing's happening"
+
+class SelectFromModelTransformer:
+    def __init__(self):
+        self.clf = RandomForestRegressor(n_estimators=50,n_jobs=-1)
+    def fit(self, X, y):
+        self.clf.fit(X,y)
+        self.model = SelectFromModel(self.clf, prefit=True)
+        return self.model
+    def transform(self,X):
+        return self.model.transform(X)
+    def __str__(self) :
+        return " Probably not the best one"
+    
+class VarianceThresholdTransformer:
+    def __init__(self):
+        self.preprocessor = VarianceThreshold(threshold=50.0)  
+    def fit(self, X, y):
+        self.preprocessor.fit(X,y)
+        return self.preprocessor
+    def transform(self,X):
+        return self.preprocessor.transform(X)
+    def __str__(self) :
+        return " VarianceThreshold=50"
+    
+class PCATransformer:
+    def __init__(self):
+        self.preprocessor = PCA(n_components = 17,svd_solver = 'auto') 
+    def fit(self, X, y):
+        self.preprocessor.fit(X,y)
+        return self.preprocessor
+    def transform(self,X):
+        return self.preprocessor.transform(X)
+    def __str__(self) :
+        return " VarianceThreshold=10"
+
 
 class Preprocessor(BaseEstimator):
     def __init__(self):
+        '''
+        This example does not have comments: you must add some.
+        Add also some defensive programming code, like the (calculated) 
+        dimensions of the transformed X matrix.
+        '''
         self.transformer = NothingTransformer()
         print("PREPROCESSOR=" + self.transformer.__str__())
 
     def fit(self, X, y=None):
-        return self.transformer.fit(X, y)
+        print("PREPRO FIT")
+        self.transformer.fit(X, y)
+        return self.transformer
 
-    def fit_transform(self, X, y=None):
-        return self.transformer.fit_transform(X)
+# =============================================================================
+#     def fit_transform(self, X, y=None):
+#         print("PREPRO FIT_TRANSFORM")
+#         return self.transformer.fit_transform(X)
+# =============================================================================
 
     def transform(self, X, y=None):
+        print("PREPRO TRANSFORM")
         return self.transformer.transform(X)
     
-    def selectFeatures(self, X, y=None):
-        clf = BaggingRegressor(base_estimator=RandomForestRegressor(n_estimators=50))
-        clf = clf.fit(X, y)
-        model = SelectFromModel(clf, prefit=True)
-        return model
-    
-    def pipe(self, X, y=None):
-         estimators = [('imputer',Imputer()),('scaler',StandardScaler()),
-                       ('red_dim', LocallyLinearEmbedding()),
-                       ('clustring',FeatureAgglomeration())]
-         pipe = Pipeline(estimators)
-         pipe.fit(X, y)
-         return pipe
-
 if __name__=="__main__":
-    # We can use this to run this file as a script and test the Preprocessor
+    # Put here your OWN test code
+    
+    # To make sure this runs on Codalab, put here things that will not be executed on Codalab
+    from sys import argv, path
+    path.append ("../ingestion") # Contains libraries you will need
+    from data_manager import DataManager  # such as DataManager
+    
     if len(argv)==1: # Use the default input and output directories if no arguments are provided
-        input_dir = "../../sample_data"
-        output_dir = "../../results" # Create this directory if it does not exist
+        input_dir = "../../public_data" # Replace by correct path
+        output_dir = "../results" # Create this directory if it does not exist
     else:
         input_dir = argv[1]
         output_dir = argv[2];
     
-    basename = 'Housing'
+    basename = 'houseprice'
     D = DataManager(basename, input_dir) # Load data
     print("*** Original data ***")
     print D
-    # Starting preprocessing
+    
     Prepro = Preprocessor()
-    # Defining variables
-    X=np.copy(D.data['X_train'])
-    y=np.copy(D.data['Y_train'])
-    x_valid=np.copy(D.data['X_valid'])
-    x_test=np.copy(D.data['X_valid'])
-    # Selection of features and transformation
-    model_selection = Prepro.selectFeatures(X, y)
-    D.data['X_train'] = model_selection.transform(X)
-    D.data['X_valid']=model_selection.transform(x_valid)
-    D.data['X_test']=model_selection.transform(x_test)
-    estimators = [('imputer',Imputer()),('scaler',MinMaxScaler()),('clustring',FeatureAgglomeration())]
-    pipe = Pipeline(estimators)
-    D.data['X_train']=pipe.fit_transform(D.data['X_train'],D.data['Y_train'])
-    D.data['X_valid']=pipe.transform(D.data['X_valid'])
-    D.data['X_test']=pipe.transform(D.data['X_test'])
+ 
+    # Preprocess on the data and load it back into D
+    D.data['X_train'] = Prepro.fit_transform(D.data['X_train'], D.data['Y_train'])
+    D.data['X_valid'] = Prepro.transform(D.data['X_valid'])
+    D.data['X_test'] = Prepro.transform(D.data['X_test'])
   
     # Here show something that proves that the preprocessing worked fine
     print("*** Transformed data ***")
     print D
+    
